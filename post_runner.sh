fc57c439042024fc92c62c38c93b257efead0390
Isaac Velando <isaac@greplinux.com>
2017-08-21 22:53:28 +0000

    Adjust hard-coded (what was I thinking) interval

--- post_runner.sh
+++ post_runner.sh
@@ -8,7 +8,7 @@ trap 'rm -f /tmp/post.lock' EXIT
 
 # Figure out what directory this script resides in
 thisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-intervalSeconds=1799
+intervalSeconds=4799
 
 # Wait a random time within the select interval
 /bin/sleep $(( $RANDOM % $intervalSeconds ))
